f4224c2d9d8f781273f26447e13ab3c6daa4f3de
chomh96 <chomh96@hanmail.net>
2022-02-12 16:59:28 +0900

    # 코드 정렬 정리

--- code/style_transfer.py
+++ code/style_transfer.py
@@ -1,47 +1,51 @@
-# 결과 로그 상태 숨기기
+from module import parser as Parser
+import sys
+import tensorflow_hub as hub
+import PIL.Image
+import numpy as np
+import tensorflow as tf
 import os
+
+# 결과 로그 상태 숨기기
 # 0 = all messages are logged (default behavior)
 # 1 = INFO messages are not printed
 # 2 = INFO and WARNING messages are not printed
 # 3 = INFO, WARNING, and ERROR messages are not printed
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import tensorflow as tf
-
-import numpy as np
-import PIL.Image
-import tensorflow_hub as hub
-import sys
 
 # parser
-from module import parser as Parser
 option = Parser.parse_args()
+
 
 # 이미지 변환 함수
 def tensor_to_image(tensor):
-  tensor = tensor*255
-  tensor = np.array(tensor, dtype=np.uint8)
-  if np.ndim(tensor)>3:
-    assert tensor.shape[0] == 1
-    tensor = tensor[0]
-  return PIL.Image.fromarray(tensor)
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+
+    return PIL.Image.fromarray(tensor)
+
 
 # 이미지 불러오기 함수
 def load_img(path_to_img):
-  max_dim = 512
-  img = tf.io.read_file(path_to_img)
-  img = tf.image.decode_image(img, channels=3)
-  img = tf.image.convert_image_dtype(img, tf.float32)
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
 
-  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-  long_dim = max(shape)
-  scale = max_dim / long_dim
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
 
-  new_shape = tf.cast(shape * scale, tf.int32)
+    new_shape = tf.cast(shape * scale, tf.int32)
 
-  img = tf.image.resize(img, new_shape)
-  img = img[tf.newaxis, :]
-  return img
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
 
 # Main
 if __name__ == '__main__':
@@ -62,8 +66,10 @@ if __name__ == '__main__':
 
     # 이미지 가로 사이즈 변경
     if option.resize_width:
-        content_image = tf.image.resize(content_image, (option.resize_width, option.resize_width))
-        style_image = tf.image.resize(style_image, (option.resize_width, option.resize_width))
+        content_image = tf.image.resize(
+            content_image, (option.resize_width, option.resize_width))
+        style_image = tf.image.resize(
+            style_image, (option.resize_width, option.resize_width))
 
     # 모델 선택 [Default: model_v1]
     if option.model:
@@ -83,7 +89,8 @@ if __name__ == '__main__':
         # hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')  # Server Model ver 1
 
     # 이미지 변환하기
-    stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+    stylized_image = hub_module(tf.constant(
+        content_image), tf.constant(style_image))[0]
     result = tensor_to_image(stylized_image)
 
     # Output 경로 설정
